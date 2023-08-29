5269849468bc86d0623c97f4c2075e524e8731ef
mayurjungari <mayurjungari1@gmail.com>
2023-08-29 09:14:07 +0530

    Changes made in book appointment

--- book.js
+++ book.js
@@ -1,5 +1,6 @@
 function storeToLocalStorage(event) {
     event.preventDefault();
+    console.log(5);
 
     var name = document.getElementById('name').value;
     var email = document.getElementById('email').value;
@@ -10,69 +11,95 @@ function storeToLocalStorage(event) {
         email: email,
         phno: phno
     };
+    showUserOnScreen(formData);
 
-  axios.post("https://crudcrud.com/api/f110fc90dfc5476f958cab060a5d8e07/appointmentData",formData)
-  .then((respons)=>{
-    console.log(respons.data);
-  })
-  .catch((err)=>{
-    console.log(err)
-  })
+  // axios.post("https://crudcrud.com/api/f110fc90dfc5476f958cab060a5d8e07/appointmentData",formData)
+  // .then((respons)=>{
+  //   console.log(respons.data);
+  // })
+  // .catch((err)=>{
+  //   console.log(err)
+  // })
 
 
 
     var data= JSON.stringify(formData)
     localStorage.setItem(email,data);
 
-    var unorderList=document.getElementById('listitem')
-     
-    var li=document.createElement('li');
-    li.className='listy'
-    var textnode=document.createTextNode(name+"-"+email+"-"+phno);
-    li.appendChild(textnode)
-    //deletebutton
-    var deletebtn=document.createElement('button')
-    deletebtn.className='btn-dark delete'
-    deletebtn.appendChild(document.createTextNode('delete'));
-    li.appendChild(deletebtn)
-    //editbutton
-    var editbtn=document.createElement('button')
-    editbtn.className='btn-primary edit'
-    editbtn.appendChild(document.createTextNode('edit'))
-    li.appendChild(editbtn)
-   
-    unorderList.appendChild(li);
-   
-} 
-
-function deleteData(event)
-{
-    event.preventDefault();
-    var email=event.target.parentNode.textContent.split('-')[1].trim();
-   
-    var unorderList=document.getElementById('listitem')
-    if(event.target.classList.contains('delete'))
+    function showUserOnScreen (formData)
     {
-        // remove item from local storage
-       localStorage.removeItem(email)
-      
-       var li=event.target.parentElement;
-       unorderList.removeChild(li)
 
+    var unorderList=document.getElementById('listitem');
+    const li=document.createElement('li')
+    li.textContent= formData.name+'-'+formData.email+'-'+formData.phno
+//del
+    const deletebtn=document.createElement('input');
+
+    deletebtn.type="button"
+    deletebtn.value='delete';
+    deletebtn.onclick=()=>{
+      localStorage.removeItem(formData.email);
+      unorderList.removeChild(li)
+    }
+    //edi
+    const editbtn=document.createElement('input');
+    editbtn.type="button"
+    editbtn.value='edit';
+    editbtn.onclick=()=>{
+      localStorage.removeItem(formData.email);
       
       
     }
-}
-function editData(event)
-{
-    var unorderList=document.getElementById('listitem')
-    if(event.target.classList.contains('edit'))
-    var email=event.target.parentNode.textContent.split('-')[1].trim();
-    localStorage.removeItem(email)
-    var li=event.target.parentElement
-    unorderList.removeChild(li)
-    document.getElementById('name').focus();
+
+    li.appendChild(deletebtn);
+    li.appendChild(editbtn);
+    unorderList.appendChild(li)
+
+     
+   
+    // //deletebutton
+    // var deletebtn=document.createElement('button')
+    // deletebtn.className='btn-dark delete'
+    // deletebtn.appendChild(document.createTextNode('delete'));
+    // li.appendChild(deletebtn)
+    // //editbutton
+    // var editbtn=document.createElement('button')
+    // editbtn.className='btn-primary edit'
+    // editbtn.appendChild(document.createTextNode('edit'))
+    // li.appendChild(editbtn)
+   
+    // unorderList.appendChild(li);
+   
+} 
+
+// function deleteData(event)
+// {
+//     event.preventDefault();
+//     var email=event.target.parentNode.textContent.split('-')[1].trim();
+   
+//     var unorderList=document.getElementById('listitem')
+//     if(event.target.classList.contains('delete'))
+//     {
+//         // remove item from local storage
+//        localStorage.removeItem(email)
+      
+//        var li=event.target.parentElement;
+//        unorderList.removeChild(li)
+
+      
+      
+//     }
+// }
+// function editData(event)
+// {
+//     var unorderList=document.getElementById('listitem')
+//     if(event.target.classList.contains('edit'))
+//     var email=event.target.parentNode.textContent.split('-')[1].trim();
+//     localStorage.removeItem(email)
+//     var li=event.target.parentElement
+//     unorderList.removeChild(li)
+//     document.getElementById('name').focus();
    
 
    
-}
+ }
